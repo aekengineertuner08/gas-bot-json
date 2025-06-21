9f9373e44e3d71fee0c964dba6c7a0b5c4f590bc
aekengineertuner08 <aekengineertuner@gmail.com>
2025-06-21 16:40:21 +0700

    Delete handlers/handlers directory

--- handlers/handlers/notify.py
+++
@@ -1,6 +0,0 @@
-# handlers/notify.py
-from flask import jsonify
-
-def schedule_notify(request):
-    # TODO: ใส่ logic การแจ้งเตือนจากโปรเจคเดิมของคุณตรงนี้
-    return jsonify({"notify": "scheduled"}), 200
